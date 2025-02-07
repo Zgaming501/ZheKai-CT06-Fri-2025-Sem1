f1e3ec4d372d543919da0709b18a43367b708750
Zgaming501 <neolyzk@gmail.com>
2025-02-07 10:48:15 +0000

    Feb 7, 2025, 10:48 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -47,7 +47,7 @@ print("I would love to visit" + (Dream))
 Age = input("What is your age?")
 Age = int(Age)
 Age = age + 50
-print("If I am 50 years older, I would be taking a walk in the park")
+print("If I am 50 years older at ,")
 
 
 # **Task 3b**:
