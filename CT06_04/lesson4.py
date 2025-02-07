370c170d37a1138f5c41b4ed754b23daa94e697d
Zgaming501 <neolyzk@gmail.com>
2025-02-07 10:04:45 +0000

    Feb 7, 2025, 10:04 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -6,7 +6,7 @@ print("$" + str(futuremoney))
 #**Task 1a**:
 #Use the input() function to ask the user for their name and
 #store it in a variable. Then print that variable.
-Name12 = input
+Name12 = input("")
 
 #**Task 1b**:
 #Ask the user for their favorite colour using input() and
