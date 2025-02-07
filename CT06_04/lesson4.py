9a11d7da77bc366cb5c0e8009fc855421f6eed67
Zgaming501 <neolyzk@gmail.com>
2025-02-07 10:04:01 +0000

    Feb 7, 2025, 10:04 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -3,3 +3,14 @@ money = input("how much money is in your bank")
 money = int(money)
 futuremoney = money * 20
 print("$" + str(futuremoney))
+#**Task 1a**:
+#Use the input() function to ask the user for their name and
+#store it in a variable. Then print that variable.
+
+#**Task 1b**:
+#Ask the user for their favorite colour using input() and
+#store the response in a variable. Print the variable.
+
+#**Task 1c**:
+#Ask the user for their age using input() and store the answer
+#in a variable. Print the variable.
