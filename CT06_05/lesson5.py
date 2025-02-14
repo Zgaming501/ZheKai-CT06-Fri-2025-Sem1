79f8f24d8a8c50b7f57daefd239d88312bbbdf76
Zgaming501 <neolyzk@gmail.com>
2025-02-14 09:53:32 +0000

    Feb 14, 2025, 9:53 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,4 +1,5 @@
 print("Hello from lesson 5")
 Name = input("What is your name")
 Age = input("What age are you turning this year")
-Message("I am hosting a party plaese bring food")
+Message = ("I am hosting a party plaese bring food")
+print
