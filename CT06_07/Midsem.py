321a0ccbf93576add2de2d796e222695cc9686a8
Zgaming501 <neolyzk@gmail.com>
2025-03-07 09:55:33 +0000

    Mar 7, 2025, 9:55 AM

--- CT06_07/Midsem.py
+++ CT06_07/Midsem.py
@@ -1,5 +1,5 @@
 # Question 1
 Name12 = input("What is your name")
 print((Name12))
-# for counter in range (5):
-#     print("In the loop" + str(counter) )
+for counter in range (5):
+     print("In the loop" + str(counter) )
