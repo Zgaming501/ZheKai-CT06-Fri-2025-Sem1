a5e0a5c908ca579c5e079a3dfa8be0f061ff031e
Zgaming501 <neolyzk@gmail.com>
2025-03-07 10:31:57 +0000

    Mar 7, 2025, 10:31 AM

--- CT06_07/Midsem.py
+++ CT06_07/Midsem.py
@@ -1,17 +1,19 @@
-# Question 1
-# ask for name and generate a greeting message
-Name12 = input("What is your name")
-print("Nice to meet you" + (Name12) )
-Name12 = input("What is your name")
-print("Nice to meet you" + (Name12) )
-Name12 = input("What is your name")
-print("Nice to meet you" + (Name12) )
-# Question 2
-# ask for three numbers 
-# generate a sequence with a interval
-for counter in range (4,10,2):
-    print(int(counter))
-for counter in range (7,1,-3):
-    print(int(counter))
-for counter in range (77,7,0):
-    print(int(counter))
+# # Question 1
+# # ask for name and generate a greeting message
+# Name12 = input("What is your name")
+# print("Nice to meet you" + (Name12) )
+# Name12 = input("What is your name")
+# print("Nice to meet you" + (Name12) )
+# Name12 = input("What is your name")
+# print("Nice to meet you" + (Name12) )
+# # Question 2
+# # ask for three numbers 
+# # generate a sequence with a interval
+# for counter in range (4,10,2):
+#     print(int(counter))
+# for counter in range (7,1,-3):
+#     print(int(counter))
+# for counter in range (77,7,0):
+#     print(int(counter))
+for counter in range (100,1,-1):
+    priny
