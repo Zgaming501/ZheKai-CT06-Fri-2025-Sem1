e48842c07409e5b4dc93fc27808a4f77d9e0e614
Zgaming501 <neolyzk@gmail.com>
2025-03-07 09:30:46 +0000

    Mar 7, 2025, 9:30 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,5 +1,39 @@
-print("Hello from lesson 5")
-Name = input("What is your name")
-Age = input("What age are you turning this year")
-Message = ("I am hosting a party plaese bring food")
-print("Happy Age birthday Name Message")
+# print("Hello from lesson 5")
+# Name = input("What is your name")
+# Age = input("What age are you turning this year")
+# Message = ("I am hosting a party plaese bring food")
+# print("Happy Age birthday Name Message")
+
+
+
+# print("before loop")
+# for counter in range (5):
+#     print("in the loop" + str(counter))
+#     print("outside the loop")
+# name = ("taylor swift")
+# for letter in name:
+#     print("swifties for Harris" + letter)
+#task 1
+# name =input("What is your name")
+# for counter in range (5):
+#     print("In the loop" + str(counter) )
+#     print("outside the loop")
+# name = (name)
+# for letter in name:
+#     print("Give me a:" + letter)
+# for counter in range (100):
+#     print("I like chicken rice")
+# for counter in range (100):
+#     print("I like cake")
+for counter in range (100):
+    print("I like cake")
+
+for counter in range (0,59):
+    print(int(counter))
+for counter in range (1,6):
+    print(int(counter))
+for counter in range (51,101):
+    print(int(counter))
+for counter in range (8,100,8):
+    print(int(counter))
+
