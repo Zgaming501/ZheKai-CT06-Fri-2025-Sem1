55bdd7c6980071d8c610561908c49fdf1f6b3a3b
Zgaming501 <neolyzk@gmail.com>
2025-03-07 10:05:49 +0000

    Mar 7, 2025, 10:05 AM

--- CT06_07/Midsem.py
+++ CT06_07/Midsem.py
@@ -1,11 +1,11 @@
 # Question 1
 
-Name12 = input("What is your name")
-print("Nice to meet you" + (Name12) )
-Name12 = input("What is your name")
-print("Nice to meet you" + (Name12) )
-Name12 = input("What is your name")
-print("Nice to meet you" + (Name12) )
+# Name12 = input("What is your name")
+# print("Nice to meet you" + (Name12) )
+# Name12 = input("What is your name")
+# print("Nice to meet you" + (Name12) )
+# Name12 = input("What is your name")
+# print("Nice to meet you" + (Name12) )
 # Question 2
 for number in range (4,2,10):
     print(number)
