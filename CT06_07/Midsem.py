d9111d5c9677658bb0fe1b2b2d9105bc3ca80f25
Zgaming501 <neolyzk@gmail.com>
2025-03-07 10:12:44 +0000

    Mar 7, 2025, 10:12 AM

--- CT06_07/Midsem.py
+++ CT06_07/Midsem.py
@@ -7,6 +7,8 @@
 # Name12 = input("What is your name")
 # print("Nice to meet you" + (Name12) )
 # Question 2
+# ask for three numbers 
+
 for counter in range (4,10,2):
     print(int(counter))
 for counter in range (7,1,-3):
