dbc0fb702dba98372f9c7a7d74caff8824511d3f
Zgaming501 <neolyzk@gmail.com>
2025-04-04 10:00:27 +0000

    Apr 4, 2025, 10:00 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,10 +1,4 @@
-print("Hello from lesson 10")
-num = int(input("What number do you want"))
-remainder = num % 2
-if remainder == 0:
-    print("It is an even number")
-else: 
-    print("It is an odd number")
+
 age = int(input("How old are u?"))
 if age == 0:
     print("Age cannot be negative how did u even come to this place")
