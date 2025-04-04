105a708acc39741562aa17334beb2432e383b05b
Zgaming501 <neolyzk@gmail.com>
2025-04-04 09:26:38 +0000

    Apr 4, 2025, 9:26 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,1 +1,24 @@
 print("Hello from lesson 9")
+numApples = int(input("How many apples? "))
+numOranges = int(input("How many oranges? "))
+
+if numApples > 5:
+    apple = 0.6 * numApples * 0.9
+else:
+    apples = 0.6 * numApples
+
+if numOranges > 5:
+    orange = 0.9 * numOranges * 0.9
+else:
+    oranges = 0.9 * numOranges
+
+total = apples + oranges 
+print("Please pay me $" + str(total))
+
+positive_days = 0
+for count in range(7):
+    temp = int(input("What is the temperature for today? "))
+    if temp > 30:
+        posititve_days = positive_days + 1
+
+print(str(positive_days) + "days are hotter tha 30 deg outside")
--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,7 @@
 print("Hello from lesson 10")
+num = int(input("What number do you want"))
+remainder = num % 2
+if remainder == 0:
+    print("It is an even number")
+else: 
+    print("It is an odd number")
