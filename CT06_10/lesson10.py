4c7ca6dfb6e41f3afe93dbafea8a8320be732773
Zgaming501 <neolyzk@gmail.com>
2025-04-04 10:17:41 +0000

    Apr 4, 2025, 10:17 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -7,7 +7,7 @@ elif age>17:
 else:
     print("U are not of eligible age to vote yet Womp Womp")
 px = int(input("What price do you want"))
-if px < 5:
+if px <=5:
     print("Sounds good")
 elif px <=50:
     print("Are u sure if u need this item?")
