a5020d5a9a4f679b86b4176ffc5890f017cb5d9e
Zgaming501 <neolyzk@gmail.com>
2025-04-04 09:56:48 +0000

    Apr 4, 2025, 9:56 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -22,3 +22,15 @@ for count in range(7):
         posititve_days = positive_days + 1
 
 print(str(positive_days) + "days are hotter tha 30 deg outside")
+print("Hello from lesson 11")
+score = int(input("What score did you get? I am curious?"))
+if 89<score<101:
+    print("AL1")
+elif 79 <score< 90:
+    print("AL2")
+elif 69 <score< 80:
+    print("AL3")
+elif 59 <score< 70:
+    print("AL4")
+else:
+    print("AL5-AL6,AL7,AL8, try harder next time study more")
