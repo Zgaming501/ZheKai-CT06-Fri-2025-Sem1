e3b653832fb0b9fb13a2000059628f53e443051a
Zgaming501 <neolyzk@gmail.com>
2025-04-04 10:01:41 +0000

    Apr 4, 2025, 10:01 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,8 +1,8 @@
 
 age = int(input("How old are u?"))
 if age == 0:
-    print("Age cannot be negative how did u even come to this place")
-elif 0 <age< 18:
+    print("Age cannot be negative how did u even come to this place?")
+elif age>17:
     print("Not eligible to vote")
 else:
     print("U are eligible to vote")
