872ea2e1c331943eabb8721b1fc08a35d4f862b5
Zgaming501 <neolyzk@gmail.com>
2025-04-04 10:39:59 +0000

    Apr 4, 2025, 10:39 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -21,6 +21,5 @@ if (divide %3==0) and (divide %7==0):
     print("The nmuber id divisible by 3 and 7")
 else:
     print("It is not divisible by 3 and 7")
-rider1 = ("How old are you")
-rider2 = ("How old are u")
+rider1
 
