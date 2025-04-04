c31077eec8a80704dce2347d71502e67cec4319e
Zgaming501 <neolyzk@gmail.com>
2025-04-04 10:17:30 +0000

    Apr 4, 2025, 10:17 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -9,9 +9,9 @@ else:
 px = int(input("What price do you want"))
 if px < 5:
     print("Sounds good")
-elif px < 50:
+elif px <=50:
     print("Are u sure if u need this item?")
-elif px < 500:
+elif px <=500:
     print("Where does ur money come from")
-elif px > 500:
+elif px >=500:
     print("do you u really need this? If u are so rich, gimmie money$$")
