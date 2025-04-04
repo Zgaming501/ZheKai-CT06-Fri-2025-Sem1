544ef850bdd0846492f19b69dc79ce7ee3c058ce
Zgaming501 <neolyzk@gmail.com>
2025-04-04 09:52:23 +0000

    Apr 4, 2025, 9:52 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -2,11 +2,11 @@ print("Hello from lesson 11")
 score = int(input("What score did you get? I am curious?"))
 if 89<score<101:
     print("AL1")
-elif 79<score<90:
+elif 79 <score< 90:
     print("AL2")
-elif 69<score<80:
+elif 69 <score< 80:
     print("AL3")
-elif 59<score<70:
+elif 59 <score< 70:
     print("AL4")
 else:
     print("AL5, try harder next time")
