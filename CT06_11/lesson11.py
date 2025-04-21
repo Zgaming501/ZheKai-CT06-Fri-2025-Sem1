65c6d8cbbf614c1ebda839bdf0d07cc904e7b06b
Zgaming501 <neolyzk@gmail.com>
2025-04-21 10:56:52 +0000

    Apr 21, 2025, 10:56 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -10,3 +10,24 @@ elif 59 <score< 70:
     print("AL4")
 else:
     print("AL5-AL6,AL7,AL8, try harder next time study more")
+
+colour = int(input("What colour do you like?"))
+
+if not (colour=="Green"):
+    print("You are wrong, you must choose green")
+else:
+    print("You are absolutely correct. Good choice")
+day = int(input("What day of the week is it?"))
+if not (day == "Saturday"):
+    print("It is not the weekends!!")
+else:
+    print("It's the weekend! No school!")
+print("Welcome to McDonalds")
+burger = int(input("Do you want a burger?"))
+drinks = int(input("What drink do you want?"))
+fries = int(input("Do u want fries?"))
+if burger == "yes" and fries == "yes"and not drinks == "yes":
+    print("Won't you get thirsty?")
+else:
+    print("Good choice of food and drinks, your order is sent to the kitchen")
+    
--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,1 +1,38 @@
 print("Hello from lesson 12")
+word = input("give me a 5 letter word")
+
+found_e = False
+found_o = False
+
+for letter in word:
+    if letter == "e":
+        found_e = True
+    if letter == "o":
+        found_o = True
+
+if found_o and found_e:
+    print("good choice of word")
+else:
+    print("Invalid  word")
+
+visitor= 0
+while counter <50:
+    print("counter + 1")
+    counter +=1
+for counter in range (18,31,1):
+    print(int(counter))
+for counter in range (4,26,1):
+    print(int(counter))
+
+while visitor 
+
+
+
+
+
+
+
+
+
+
+order = "Fried Fish, Fries, Coke"
