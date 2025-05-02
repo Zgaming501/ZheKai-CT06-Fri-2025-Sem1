1168f00d38b4d823ffa987aca5710300bf6989e8
Zgaming501 <neolyzk@gmail.com>
2025-05-02 10:09:04 +0000

    May 2, 2025, 10:09 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -2,5 +2,5 @@ print("Hello from lesson 15")
 groceries = ["Apples", "Bread", "carrots", "Dates", "Eggs", "Flour", "grapes", "Honey"]
 groceries[7] = "Herbs"
 print(groceries)
-zoo = []
-zoo.append("Ice")
+groceries = []
+groceries.append("Ice")
