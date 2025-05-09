45e7dc9f73bd0035c0f255bbe17647852cb65d13
Zgaming501 <neolyzk@gmail.com>
2025-05-09 09:50:44 +0000

    May 9, 2025, 9:50 AM

--- CT06_13/test.py/test.py
+++ CT06_13/test.py/test.py
@@ -2,8 +2,7 @@
 # use the while loop to print numbers 10 to 200
 # print in multiples of ten
 # number 10 first and 200 last
-#while number in range(9,201):
-    print(int(counter))
+#while number in range(9,201)print(int(counter))
 #task 2
 #password checker to protect superpass123
 # ask user for password
